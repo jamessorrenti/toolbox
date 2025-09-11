41c50adc1a8462e0afef9bde92211ff2596cd1c8
jamessorrenti <github@jamessorrenti.com>
2025-09-11 13:19:12 -0400

    Fixing date string

--- dock_deployer/dock-deployer.zsh
+++ dock_deployer/dock-deployer.zsh
@@ -246,7 +246,7 @@ mobileconfig = {
     "PayloadVersion": 1,
     "PayloadIdentifier": f"com.example.dock.export.{profile_uuid}",
     "PayloadUUID": profile_uuid,
-    "PayloadDisplayName": f"Dock Deploy ({datetime.datetime.now().strftime('%Y-%m-%d')})",
+    "PayloadDisplayName": "Dock Deploy (" + datetime.datetime.now().strftime('%Y-%m-%d') + ")",
     "PayloadRemovalDisallowed": False,
     "PayloadContent": [payload],
 }
